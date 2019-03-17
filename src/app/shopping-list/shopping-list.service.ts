6e5a3e9f0c6ab7b9630042a7177472caf05465e3
lisw4y <lsw99u@gmail.com>
2019-03-17 14:03:35 -0700

    Add recipe and shopping list services to store data and provide methods

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,33 @@
+import { EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    for (let ingredient of ingredients) {
+      const oldIngredient = this.ingredients.find(ing => ing.name === ingredient.name);
+      if (oldIngredient) {
+        oldIngredient.amount += ingredient.amount;
+      } else {
+        this.ingredients.push(ingredient);
+      }
+    }
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+}
